75c1b4cc3dfab71708594acc77b8c13d3c3ee0ee
GuyKroizman <kroizguy@gmail.com>
2017-01-20 16:16:17 +0200

    rotattion

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class Controller : MonoBehaviour {
 
     private Animator animator;
+    private Vector3 rotationDirection;
+    public int rotationSpeed;
 
     
     void Start () {
         animator = GetComponent<Animator>();
+        rotationDirection = Vector3.back * rotationSpeed;
 
     }
 	
 	// Update is called once per frame
 	void Update () {
+        transform.Rotate(rotationDirection);
         if (Input.GetKeyDown(KeyCode.Space) == true)
-        {            
-            if(animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !animator.IsInTransition(0))
-            {
-                animator.Play("Fire");
-            }
+        {
+            
+            rotationDirection *= -1;
+            //if(animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !animator.IsInTransition(0))
+            //{
+                //animator.Play("Fire");
+            //}
         }
             
         
