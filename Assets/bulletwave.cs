9bdb9f0382d709d663513399deaacd492c2e042b
J <J@J.J>
2017-01-20 18:52:43 +0200

    Made waves travel 'right'.

---
+++ Assets/bulletwave.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletwave : MonoBehaviour {
+
+	public int speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (transform.right * Time.deltaTime * speed);
+	}
+}
