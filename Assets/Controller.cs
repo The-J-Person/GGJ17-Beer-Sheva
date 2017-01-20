87da5c90832c2642ffa232155ac37a097e2b0a8f
GuyKroizman <kroizguy@gmail.com>
2017-01-20 15:52:02 +0200

    fire animation

---
+++ Assets/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    private Animator animator;
+
+    
+    void Start () {
+        animator = GetComponent<Animator>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {            
+            if(animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !animator.IsInTransition(0))
+            {
+                animator.Play("Fire");
+            }
+        }
+            
+        
+
+
+    }
+}
