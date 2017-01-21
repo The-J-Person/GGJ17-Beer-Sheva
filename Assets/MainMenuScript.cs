2577cd936732756d4992afa1a293266c493300a5
GuyKroizman <kroizguy@gmail.com>
2017-01-21 20:56:57 +0200

    style main mainmenu

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour {
+
+    public void onClick()
+    {
+        Application.LoadLevel(1);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
